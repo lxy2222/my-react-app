be70631c0b22c961fc05197ee94a055ac87d9984
梁晓宇 <liangxiaoyu02@meituan.com>
2017-04-26 18:45:24 +0800

    修改

--- index.js
+++ index.js
@@ -1,1 +1,56 @@
-index.js
+var fs = require('fs');
+var path = require('path');
+var express = require('express');
+var bodyParser = require('body-parser');
+var app = express();
+var COMMENTS_FILE = path.join(__dirname, 'comments.json');
+// 设置端口
+app.set('port', (process.env.PORT || 3000));
+// 托管静态资源
+// 访问根目录的时候，
+app.use('/', express.static(path.join(__dirname, 'src')));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
+// 设置请求防止跨域
+app.use(function(req, res, next) {
+	res.setHeader('Access-Control-Allow-Origin', '*');
+	res.setHeader('Cache-Control', 'no-cache');
+	next();
+});
+app.get('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    res.json(JSON.parse(data));
+  });
+});
+app.post('/api/comments', function(req, res) {
+  fs.readFile(COMMENTS_FILE, function(err, data) {
+    if (err) {
+      console.error(err);
+      process.exit(1);
+    }
+    var comments = JSON.parse(data);
+    // NOTE: In a real implementation, we would likely rely on a database or
+    // some other approach (e.g. UUIDs) to ensure a globally unique id. We'll
+    // treat Date.now() as unique-enough for our purposes.
+    var newComment = {
+      id: Date.now(),
+      author: req.body.author,
+      text: req.body.text,
+    };
+    comments.push(newComment);
+    fs.writeFile(COMMENTS_FILE, JSON.stringify(comments, null, 4), function(err) {
+      if (err) {
+        console.error(err);
+        process.exit(1);
+      }
+      res.json(comments);
+    });
+  });
+});
+app.listen(app.get('port'), function() {
+  console.log('Server started: http://localhost:' + app.get('port') + '/');
+});
